f03d0bb61f7b2a72e19fe3a91358a6c024873e13
WKHAllen <wkhallen@gmail.com>
2020-02-11 13:41:20 -0600

    Link pthread to shared object

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 mkdir bin/obj 2>/dev/null
 gcc -c -Wall -Werror -fpic src/*.c
 mv ./*.o bin/obj/
-gcc -shared -o bin/libcdtp.so bin/obj/*.o
+gcc -shared -o bin/libcdtp.so bin/obj/*.o -lpthread
 mkdir bin/include 2>/dev/null
 cp src/*.h bin/include/
